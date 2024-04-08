2bbf2393d9a593ce20969c233dafe5e70ee00dc1
s223200581 <142364255+s223200581@users.noreply.github.com>
2024-04-08 18:57:31 +1000

    Create secret.h

---
+++ secret.h
@@ -0,0 +1,9 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "OPTUS_B564B8N"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "benne69725vk"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2498030			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "BI91XAR9LUVQK70P"   // replace XYZ with your channel write API Key
+
